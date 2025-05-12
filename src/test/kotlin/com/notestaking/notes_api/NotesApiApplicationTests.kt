62d92a28557db5fdc6f2b25c51f135e6c2e617c7
an-100n <anion.lazarev@gmail.com>
2025-05-12 20:17:56 +0200

    first commit

---
+++ src/test/kotlin/com/notestaking/notes_api/NotesApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.notestaking.notes_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotesApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
