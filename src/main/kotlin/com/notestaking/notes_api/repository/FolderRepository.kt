86eee95f982458575e65d4609d4b0da3ebc49abd
an-100n <anion.lazarev@gmail.com>
2025-05-20 14:18:18 +0200

    created the Note Repository

--- src/main/kotlin/com/notestaking/notes_api/repository/FolderRepository.kt
+++ src/main/kotlin/com/notestaking/notes_api/repository/FolderRepository.kt
@@ -1,6 +1,20 @@
 package com.notestaking.notes_api.repository
 
+import com.notestaking.notes_api.entity.FolderEntity
+import com.notestaking.notes_api.entity.NoteEntity
+import jakarta.transaction.Transactional
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.Optional
+import java.util.UUID
 
-interface FolderRepository: JpaRepository {
+interface FolderRepository : JpaRepository<FolderEntity, UUID> {
+
+    @Transactional
+    fun deleteByIdAndUser_Id(id: UUID, user_id: UUID)
+
+    fun getFoldersByUser_Id(user_id: UUID): Optional<List<NoteEntity>>
+
+    //Optional<List<NoteEntity>> getNotesByIdAndUserId(UUID id, UUID user_id);
+
+
 }
---
+++ src/main/kotlin/com/notestaking/notes_api/repository/NoteRepository.kt
@@ -0,0 +1,15 @@
+package com.notestaking.notes_api.repository
+
+import com.notestaking.notes_api.entity.NoteEntity
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.Optional
+import java.util.UUID
+
+interface NoteRepository : JpaRepository<NoteEntity, UUID> {
+
+    fun findByFolderIdAndOwner_Id(folderId: UUID, userId: UUID): Optional<NoteEntity>
+
+    fun findByIdAndOwner_Id(id: UUID, userId: UUID): Optional<NoteEntity>
+
+    fun deleteByIdAndOwner_Id(id: UUID, userId: UUID)
+}
