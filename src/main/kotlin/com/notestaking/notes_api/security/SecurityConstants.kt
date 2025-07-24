e9ac593b5a3eed89128ca06a6f7c28e805a14780
an-100n <anion.lazarev@gmail.com>
2025-07-24 13:32:25 +0200

    updated the variables in securityConstats file

--- src/main/kotlin/com/notestaking/notes_api/security/SecurityConstants.kt
+++ src/main/kotlin/com/notestaking/notes_api/security/SecurityConstants.kt
@@ -4,6 +4,6 @@ object SecurityConstants {
     const val EXPIRATION_TIME = 86_400_000L // 1 day in ms
     const val TOKEN_PREFIX = "Bearer "
     const val HEADER_STRING = "Authorization"
-    const val REGISTER_PATH = "/api/users/register"
-    const val LOGIN_PATH = "/api/users/login"
+    const val REGISTER_PATH = "/api/v1/users/register"
+    const val LOGIN_PATH = "/api/v1/users/login"
 }
